eacce4fb50f3b2ba12d77701d9ed7c8835dbabf9
bvcvbcvncxzcvzxvc <132753232+bvcvbcvncxzcvzxvc@users.noreply.github.com>
2025-04-02 13:13:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import os
-import subprocess
 import time
 from telethon import TelegramClient, events
+from telethon.sessions import StringSession
 
+# Retrieve environment variables
+try:
+    API_ID = int(os.environ['API_ID'])
+except KeyError:
+    raise KeyError("The API_ID environment variable is not set.")
 
-# Environment Variables
-API_ID = int(os.getenv('API_ID'))
-API_HASH = os.getenv('API_HASH')
-BOT_USERNAME = os.getenv('BOT_USERNAME', '@se36We')
-LICENSE_KEY = os.getenv('LICENSE_KEY', '123')
-SESSION_STRING = os.getenv('SESSION_STRING')
+API_HASH = os.environ.get('API_HASH')
+BOT_USERNAME = os.environ.get('BOT_USERNAME')
+LICENSE_KEY = os.environ.get('LICENSE_KEY')
+SESSION_STRING = os.environ.get('SESSION_STRING')
 
-
-def check_telegram_installed():
-    try:
-        subprocess.run(['telegram-desktop', '-version'], check=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
+# Check if essential environment variables are set
+if not all([API_HASH, BOT_USERNAME, LICENSE_KEY, SESSION_STRING]):
+    raise ValueError("One or more required environment variables (API_HASH, BOT_USERNAME, LICENSE_KEY, SESSION_STRING) are missing.")
 
 def verify_license():
     while True:
@@ -28,9 +27,9 @@ def verify_license():
         else:
             print('Invalid license key. Please try again.')
 
-
 def send_message_to_telegram():
-    client = TelegramClient('session_name', API_ID, API_HASH)
+    # Use the session string
+    client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
     client.start()
 
     async def main():
@@ -39,25 +38,19 @@ def send_message_to_telegram():
 
     client.loop.run_until_complete(main())
 
-
 def save_log():
     with open('license_log.txt', 'w') as file:
         file.write(f'License: {LICENSE_KEY}\n')
-        file.write(f'Code: 55888\n')
-        file.write(f'Serial: 5988888\n')
+        file.write('Code: 55888\n')
+        file.write('Serial: 5988888\n')
     print('Log file generated successfully.')
 
-
 def main():
-    if not check_telegram_installed():
-        print('Telegram Desktop is not installed. Please install it and try again.')
-        return
-
+    # We don't need to check telegram-desktop installation in a server environment
     if verify_license():
         send_message_to_telegram()
         time.sleep(5)  # Waiting for a response from the Telegram bot
         save_log()
-
 
 if __name__ == '__main__':
     main()
